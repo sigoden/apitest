67fc4727f45977b599702cf38d1aedcbb2c42d3d
sigoden <sigoden@gmail.com>
2021-06-04 23:17:34 +0800

    chore: rename createReq and compareRes

--- tests/req.test.js
+++ tests/req.test.js
@@ -1,18 +1,18 @@
 const { spwanTest } = require("./utils");
 
-describe("createReq", () => {
+describe("req", () => {
   test("eval", async () => {
-    const { stdout, code } = await spwanTest("createReq/eval.jsona", ["--ci"]);
+    const { stdout, code } = await spwanTest("req/eval.jsona", ["--ci"]);
     expect(code).toEqual(1);
     expect(stdout).toMatchSnapshot();
   });
   test("main", async () => {
-    const { stdout, code } = await spwanTest("createReq", ["--ci"]);
+    const { stdout, code } = await spwanTest("req", ["--ci"]);
     expect(code).toEqual(0);
     expect(stdout).toMatchSnapshot();
   });
   test("mock", async () => {
-    const { stdout, code } = await spwanTest("createReq/mock.jsona", ["--ci"]);
+    const { stdout, code } = await spwanTest("req/mock.jsona", ["--ci"]);
     expect(code).toEqual(1);
     expect(stdout).toMatchSnapshot();
   });
--- tests/res.test.js
+++ tests/res.test.js
@@ -1,33 +1,33 @@
 const { spwanTest } = require("./utils");
 
-describe("compareRes", () => {
+describe("res", () => {
   test("data", async () => {
-    const { stdout, code } = await spwanTest("compareRes/data.jsona", ["--ci"]);
+    const { stdout, code } = await spwanTest("res/data.jsona", ["--ci"]);
     expect(code).toEqual(1);
     expect(stdout).toMatchSnapshot();
   });
   test("eval", async () => {
-    const { stdout, code } = await spwanTest("compareRes/eval.jsona", ["--ci"]);
+    const { stdout, code } = await spwanTest("res/eval.jsona", ["--ci"]);
     expect(code).toEqual(1);
     expect(stdout).toMatchSnapshot();
   });
   test("every", async () => {
-    const { stdout, code } = await spwanTest("compareRes/every.jsona", ["--ci"]);
+    const { stdout, code } = await spwanTest("res/every.jsona", ["--ci"]);
     expect(code).toEqual(1);
     expect(stdout).toMatchSnapshot();
   });
   test("partial", async () => {
-    const { stdout, code } = await spwanTest("compareRes/partial.jsona", ["--ci"]);
+    const { stdout, code } = await spwanTest("res/partial.jsona", ["--ci"]);
     expect(code).toEqual(0);
     expect(stdout).toMatchSnapshot();
   });
   test("some", async () => {
-    const { stdout, code } = await spwanTest("compareRes/some.jsona", ["--ci"]);
+    const { stdout, code } = await spwanTest("res/some.jsona", ["--ci"]);
     expect(code).toEqual(1);
     expect(stdout).toMatchSnapshot();
   });
   test("type", async () => {
-    const { stdout, code } = await spwanTest("compareRes/type.jsona", ["--ci"]);
+    const { stdout, code } = await spwanTest("res/type.jsona", ["--ci"]);
     expect(code).toEqual(1);
     expect(stdout).toMatchSnapshot();
   });
